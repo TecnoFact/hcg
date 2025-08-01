6530c195b38b7aa673f33cbaacc08989007340ce
Gustavo Herrera <gustavoh.2312@gmail.com>
2025-07-31 20:29:36 -0400

    move chart dashboard

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -46,8 +46,9 @@ class AdminPanelProvider extends PanelProvider
 
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
+                 CfdiChart::class,
                 TotalInvoiceWidget::class,
-                CfdiChart::class,
+
             ])
             ->middleware([
                 EncryptCookies::class,
