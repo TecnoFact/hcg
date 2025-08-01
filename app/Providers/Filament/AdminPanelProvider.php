58d1e43136ebb2fa842216037c82433a678cd93c
Gustavo Herrera <gustavoh.2312@gmail.com>
2025-07-31 20:13:02 -0400

    remove account widget

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -46,7 +46,6 @@ class AdminPanelProvider extends PanelProvider
 
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
-                Widgets\AccountWidget::class,
                 TotalInvoiceWidget::class,
                 CfdiChart::class,
             ])
