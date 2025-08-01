ccdf0172a115ca22ea75db4c5d5a795ab3c5e613
Gustavo Herrera <gustavoh.2312@gmail.com>
2025-07-31 20:11:50 -0400

    fix variable data

--- app/Filament/Widgets/CfdiChart.php
+++ app/Filament/Widgets/CfdiChart.php
@@ -62,7 +62,7 @@ protected function getData(): array
         $resultados = Cfdi::query()
             ->selectRaw('DAY(created_at) as dia, COUNT(*) as total')
             ->whereBetween('created_at', [$inicioMes, $finMes])
-            ->when($estado, fn($q) => $q->where('estatus_upload', $estado))
+            ->when($estado, fn($q) => $q->where('status_upload', $estado))
             ->groupBy('dia')
             ->get()
             ->mapWithKeys(function($item) {
